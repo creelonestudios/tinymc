6d47f342c54ba53179a3547b1f34d749f6a75b8e
j0code <jonaslp759@gmail.com>
2024-01-22 21:08:19 +0100

    change testing hotbar - just one of each (except stone: 4) - remove duplicates

--- src/player.ts
+++ src/player.ts
@@ -30,11 +30,11 @@ export default class Player extends Entity {
 		this.size.set(1.5, 1.5)
 
 		// for testing, temp
-		this.hotbar.set(0, new ItemStack(new Item("tiny:stone"), 128))
-		this.hotbar.set(1, new ItemStack(new Item("tiny:dirt"), 128))
-		this.hotbar.set(2, new ItemStack(new Item("tiny:water"), 128))
-		this.hotbar.set(3, new ItemStack(new Item("tiny:dirt")))
-		this.hotbar.set(4, new ItemStack(new Item("tiny:dirt")))
+		this.hotbar.set(0, new ItemStack(new Item("tiny:stone"), 4))
+		this.hotbar.set(1, new ItemStack(new Item("tiny:dirt")))
+		this.hotbar.set(2, new ItemStack(new Item("tiny:water")))
+		this.hotbar.set(3, new ItemStack(new Item("tiny:grass_block")))
+		this.hotbar.set(4, new ItemStack(new Item("tiny:air")))
 	}
 
 	get texture() {
